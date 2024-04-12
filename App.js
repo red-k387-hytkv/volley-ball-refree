70827f25bf73f2cece19f905b4d7c4c86820be07
barisan387 <red.k387.hytkv@gmail.com>
2024-04-12 22:22:25 +0900

    Initial Commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInputBase, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>さあ審判アプリ作るぞ</Text>
+      <Text>さあ審判アプリ作るぞ</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
